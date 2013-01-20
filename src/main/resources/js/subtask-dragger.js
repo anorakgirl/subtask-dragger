d2198754c172b8bbd130fa9a20fe0a85c2b6e3b1
anorakgirl <mail@anorakgirl.co.uk>
2013-01-20 08:56:14 +0000

    initial commit

---
+++ src/main/resources/js/subtask-dragger.js
@@ -0,0 +1,47 @@
+/*
+* jQuery drag and drop functionality for Jira subtasks
+* Copyright anorakgirl 2013
+*/
+var updatePosition = function(e, ui) {
+	var url = ui.item.find("div.subtask-reorder").children("a").attr("href");
+	var currentPos = getCurrentPosition(url);
+	var newPos = 0;
+
+	var nextUrl = ui.item.next().find("div.subtask-reorder").children("a").attr("href");
+	var prevUrl = ui.item.prev().find("div.subtask-reorder").children("a").attr("href");	
+
+	//if there is a position below
+	if (nextUrl != null) {	
+		var nextPos = getCurrentPosition(nextUrl);
+		//if we are moving up, use the below index
+		if (nextPos < currentPos) {
+			newPos = nextPos;
+		} else {
+			//use the prev index as we are moving down
+			newPos = getCurrentPosition(prevUrl);			
+		}
+	} else {
+		//we must be moving down
+		newPos = getCurrentPosition(prevUrl);		
+	}
+	
+	//replace the new position with the one we worked out
+	var newUrl = url.replace(/subTaskSequence=\d*/,"subTaskSequence="+newPos);
+	
+	//go to this page
+	window.location.href = newUrl;
+	
+};
+
+function getCurrentPosition(url) {
+	var params = /.*currentSubTaskSequence=(\d)*&.*/g.exec(url);
+	return params[1];
+	
+}
+
+AJS.$(document).ready(function() {
+	AJS.$("#issuetable tbody").sortable({
+		stop: updatePosition
+	});
+});
+
